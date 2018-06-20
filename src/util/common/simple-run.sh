64eb5f01e59fc0978d9e723ef721fbb22ed89346
Ahn, Seongjin <jam45@naver.com>
2018-06-20 02:40:56 +0000

    Fixed error due to a typo in simple-run.sh

--- src/util/common/simple-run.sh
+++ src/util/common/simple-run.sh
@@ -7,5 +7,5 @@ $QPROP_HOME/bin/imag-prop > im.log 2>&1
 echo "[$(date)] real-prop started"
 $QPROP_HOME/bin/real-prop > re.log 2>&1
 echo "[$(date)] eval-tsurff-mpi started"
-$QPROP_DEP_DIR/openmpi/bin/mpiexec -np $num_of_prcess $QPROP_HOME/bin/eval-tsurff-mpi  > eval.log 2>&1
+$QPROP_DEP_DIR/openmpi/bin/mpiexec -np $num_of_process $QPROP_HOME/bin/eval-tsurff-mpi  > eval.log 2>&1
 

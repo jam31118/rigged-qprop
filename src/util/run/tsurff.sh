55c94fc346229598575f14175493e047c14dd921
Ahn, Seongjin <jam45@naver.com>
2018-06-23 19:44:42 +0900

    Added combining partial data files by MPI processes at the end of run and SLURM batch scripts

--- src/util/run/tsurff.sh
+++ src/util/run/tsurff.sh
@@ -9,3 +9,7 @@ echo "${LOG} num_of_process: $num_of_process"
 echo "${LOG}[$(date)] eval-tsurff-mpi started"
 $QPROP_DEP_DIR/openmpi/bin/mpiexec -np $num_of_process $QPROP_HOME/bin/eval-tsurff-mpi  > eval.log 2>&1
 
+
+## Combine partial data files produced by separate MPI processes
+source $QPROP_HOME/src/util/data/combine_tsurff_data.sh
+

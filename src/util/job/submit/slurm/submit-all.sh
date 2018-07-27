96797b86ad3added48752b03628ac266f0685a4e
Ahn, Seongjin <jam45@naver.com>
2018-07-27 10:38:01 +0900

    Upgraded job submission scripts for SLURM

--- src/util/job/submit/slurm/submit-all.sh
+++ src/util/job/submit/slurm/submit-all.sh
@@ -1,15 +1,31 @@
 #! /bin/bash
 
-# Define submission scripts
-script_submit_im_re="$QPROP_HOME/src/util/job-submit/slurm/imag-real.slurm"
-script_submit_tsurff="$QPROP_HOME/src/util/job-submit/slurm/tsurff.slurm"
+## Source required scripts
+source $QPROP_HOME/src/util/shell/colors.sh
 
-if [ ! -f $script_submit_im_re ]; then exit; fi
-if [ ! -f $script_submit_tsurff ]; then exit; fi
+## Get sbatchargument for tsurff calculation
+tsurff_submit_arg="$@"
+echo -e "${LOG} Given tsurff_submit_arg: $tsurff_submit_arg"
 
-# Submit imag and real propagation
-mesg_im_re=$(sbatch $script_submit_im_re "$@")
+## Define submission scripts
+SCRIPT_DIR="$QPROP_HOME/src/util/job/submit/slurm"
+script_submit_im_re="$SCRIPT_DIR/imag-real.slurm"
+script_submit_tsurff="$SCRIPT_DIR/tsurff.slurm"
+
+## Check whether the script files exist
+if [ ! -f $script_submit_im_re ]; then (2>&1 echo -e "${ERROR} Could not find script: $script_submit_im_re"); exit 1; fi
+if [ ! -f $script_submit_tsurff ]; then (2>&1 echo -e "${ERROR} Could not fine script: $script_submit_tsurff"); exit 1; fi
+
+## Submit imag and real propagation
+# submit imag-real
+mesg_im_re=$(sbatch $script_submit_im_re)
+if [ "$?" -ne 0 ]; then (2>&1 echo -e "${ERROR} Failed to submit $script_submit_im_re"); fi
 jid_im_re=$(echo $mesg_im_re | grep -oh "[1-9][0-9]*$")
-mesg_tsurff=$(sbatch --dependency=afterok:$jid_im_re $script_submit_tsurff "$@")
+echo -e "${OK} [$(date)] Submitted $script_submit_im_re with job id: $jid_im_re"
+
+# submit tsurff
+mesg_tsurff=$(sbatch $tsurff_submit_arg --dependency=afterok:$jid_im_re $script_submit_tsurff)
+if [ "$?" -ne 0 ]; then (2>&1 echo -e "${ERROR} Failed to submit $script_submit_tsurff"); fi
 jid_tsurff=$(echo $mesg_tsurff | grep -oh "[1-9][0-9]*$")
+echo -e "${OK} [$(date)] Submitted $script_submit_tsurff with job id: $jid_tsurff"
 

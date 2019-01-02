4644c99731dc0fe86c82750edc5500f89bbcfe26
jam31118 <jam45@naver.com>
2019-01-02 17:03:53 +0900

    modified makefile to enable compilation of PPP

--- src/main/variables.mk
+++ src/main/variables.mk
@@ -11,3 +11,9 @@ gslargs=-I$(GSLHOME)/include -L$(GSLHOME)/lib -lgsl -lgslcblas
 mpiargs=-I$(MPIHOME)/include -L$(MPIHOME)/lib -lmpi -lmpi_cxx -DHAVE_MPI
 boostargs=-I$(BOOSTHOME)/include -L$(BOOSTHOME)/lib
 
+QPROP_FLAGS = -L$(QPROP_HOME)/lib/x86_64 -I$(QPROP_HOME)/src/base -lqprop
+
+MATRIX_HOME = $(QPROP_HOME)/dep/matrix
+MATRIX_FLAGS = -L$(MATRIX_HOME)/lib -I$(MATRIX_HOME)/include -lmatrix
+
+

df97a4d2d471459a84c18fb7ef0bd0a66a0c2d06
Ahn Seongjin <jam45@naver.com>
2019-01-06 22:35:17 +0900

    add ppp examples

---
+++ src/example/ppp/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+num_of_process=4
+$QPROP_HOME/bin/imag-prop
+$QPROP_HOME/bin/real-prop
+$QPROP_DEP_DIR/openmpi/bin/mpiexec -n $num_of_process $QPROP_HOME/bin/ppp
+$QPROP_DEP_DIR/openmpi/bin/mpiexec -n $num_of_process $QPROP_HOME/bin/eval-tsurff-mpi
+bash /home/ahn/qprop/rigged-qprop/src/util/data/combine_tsurff_data.sh
+
+## NOTES
+# The only diference between this example and the original examples
+# is the presence of `use-ppp` parameter in `tsurff.param` file, 
+# which configures `real-prop` program to stop propagation as soon as the laser field becomes off, 
+# leaving the field-free post-progation to be done by `ppp`.
+

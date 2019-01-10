b5769985f13f1ecd6f6b5afff5cde1c5fd89806a
Ahn, Seongjin <jam45@naver.com>
2019-01-10 14:42:57 +0000

    improve makefile and variables.mk

--- src/main/variables.mk
+++ src/main/variables.mk
@@ -3,9 +3,9 @@ arch_flags = -mtune=$(cpu_type)
 optimargs=-O3 $(arch_flags) -ffast-math
 
 LIBHOME=$(QPROP_DEP_DIR)
-GSLHOME=$(LIBHOME)/gsl/
-MPIHOME=$(LIBHOME)/openmpi/
-BOOSTHOME=$(LIBHOME)/boost/
+GSLHOME=$(LIBHOME)/gsl
+MPIHOME=$(LIBHOME)/openmpi
+BOOSTHOME=$(LIBHOME)/boost
 
 gslargs=-I$(GSLHOME)/include -L$(GSLHOME)/lib -lgsl -lgslcblas
 mpiargs=-I$(MPIHOME)/include -L$(MPIHOME)/lib -lmpi -lmpi_cxx

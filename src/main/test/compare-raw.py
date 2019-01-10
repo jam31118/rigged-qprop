c10d85491995cd7e068b0e5a0f080d2df686b604
Ahn, Seongjin <jam45@naver.com>
2019-01-10 13:46:39 +0000

    add some more funcitonality in comparison script

--- src/main/test/compare-raw.py
+++ src/main/test/compare-raw.py
@@ -5,11 +5,15 @@ psi_arr_new, psi_arr_qprop = (np.fromfile(file_path, dtype=complex) for file_pat
 are_close_arr = np.isclose(psi_arr_new, psi_arr_qprop)
 all_close = np.all(are_close_arr)
 
+print("comparing `{}` and `{}`".format(psi_new, psi_qprop))
+
 print("new: ", psi_arr_new)
 print("original: ", psi_arr_qprop)
 print("diff: ", psi_arr_new - psi_arr_qprop)
 
 print(are_close_arr)
 print("all close: {}".format(all_close))
+print("`{}` out of `{}` elements are close enough".format(are_close_arr.sum(), are_close_arr.size))
 
+print("indice of not close elements: {}".format(np.arange(are_close_arr.size)[~are_close_arr]))
 

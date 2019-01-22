bb7bce59725474a36d32a94466f8870892fa4b4a
Ahn, Seongjin <jam45@naver.com>
2019-01-22 04:47:10 +0000

    add compilation step in the test-script

--- src/main/test/prepare-test.sh
+++ src/main/test/prepare-test.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+source $QPROP_HOME/src/util/shell/colors.sh
+
+make -j4 -C $QPROP_HOME/src/main
+exit_code_make=$?
+if [ "$exit_code_make" -eq "0" ]
+then echo "${LOG} Compliation done.";
+else (2>&1 echo "${ERROR} Compilation failed\n"); exit -1;
+fi
+
 cd ./ati
 $QPROP_HOME/bin/imag-prop > im.log 2>&1
 $QPROP_HOME/bin/real-prop > re.log 2>&1 &
